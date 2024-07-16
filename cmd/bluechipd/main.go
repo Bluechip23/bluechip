e5a21fc72f3f4007f81ed4cfaff72c623dd83c96
Nick Davis <nick@bluechip.com.co>
2024-07-16 16:52:45 -0500

    adding a version tag to recent blockchain version

--- cmd/bluechipd/main.go
+++ cmd/bluechipd/main.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"os"
+    "fmt"
+    "os"
 
-	"github.com/BlueChip23/bluechip/app"
-	"github.com/cosmos/cosmos-sdk/server"
-
-	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+    "github.com/BlueChip23/bluechip/app"
+    "github.com/cosmos/cosmos-sdk/server"
+    svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
 func main() {
-	rootCmd, _ := NewRootCmd()
-	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
-		switch e := err.(type) {
-		case server.ErrorCode:
-			os.Exit(e.Code)
+    rootCmd, _ := NewRootCmd()
 
-		default:
-			os.Exit(1)
-		}
-	}
+    // Add a version command
+    rootCmd.AddCommand(&cobra.Command{
+        Use:   "version",
+        Short: "Print the version of the application",
+        Run: func(cmd *cobra.Command, args []string) {
+            fmt.Printf("Version: %s\n", Version)
+            fmt.Printf("Git Commit: %s\n", GitCommit)
+            fmt.Printf("Build Date: %s\n", BuildDate)
+        },
+    })
+
+    if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+        switch e := err.(type) {
+        case server.ErrorCode:
+            os.Exit(e.Code)
+        default:
+            os.Exit(1)
+        }
+    }
 }
---
+++ cmd/bluechipd/version.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+    // Version specifies the current version of your application.
+    Version = "v1.1.0"
+
+    // GitCommit specifies the commit hash when the binary was built.
+    GitCommit string
+
+    // BuildDate specifies the date and time when the binary was built.
+    BuildDate string
+)
